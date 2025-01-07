2553142d3a928bb813e3b3fabb7e5f531d232a9f
sankharanarayanan07 <sankharworld@gmail.com>
2025-01-07 14:02:56 +0530

    new11

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Apps from './Apps.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Apps from './Apps.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
