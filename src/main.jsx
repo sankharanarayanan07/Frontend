fd2f72cf312a1e0b0ee00c21a4c36ac3555ae147
sankharanarayanan07 <sankharworld@gmail.com>
2025-01-07 14:07:46 +0530

    new12

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import Apps from './Apps.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
